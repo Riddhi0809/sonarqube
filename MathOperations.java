79338e8fe2acbef7abd98e68a34ddf267c5f3a06
Riddhi0809 <143692390+Riddhi0809@users.noreply.github.com>
2026-01-14 08:33:25 +0530

    Add files via upload

---
+++ MathOperations.java
@@ -0,0 +1,21 @@
+public class MathOperations {
+
+    public static void main(String[] args) {
+        
+        int num1 = 20;
+        int num2 = 10;
+        
+        
+        int sum = num1 + num2;         
+        int difference = num1 - num2;   
+        int product = num1 * num2;       
+        
+              double quotient = (double) num1 / num2; 
+
+        System.out.println("Numbers used: " + num1 + " and " + num2);
+        System.out.println("Addition Result: " + sum);
+        System.out.println("Subtraction Result: " + difference);
+        System.out.println("Multiplication Result: " + product);
+        System.out.println("Division Result: " + quotient);
+    }
+}
